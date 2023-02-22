60d50b35a0880e00602c04ea2c92d3013f1ade72
Hyunkyoung Lee <HLee@1800contacts.com>
2023-02-22 15:01:59 -0700

    build WeatherUtilities

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyUtilities;
+
+namespace MyFirstProgram
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where should we go in May?");
+            WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
+            WeatherUtilities.Report("Bologna", 23, 65);
+        }
+    }
+}
